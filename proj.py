7c7cce44e88b7a8a4d0330efc939fcd8d108c961
Josh <joshua.l.fritz@gmail.com>
2022-01-20 16:30:50 -0600

    updated proj.py with tf-idf and word2vec commands.

--- proj.py
+++ proj.py
@@ -4,9 +4,11 @@ Created on Wed Jan 19 17:33:03 2022
 
 @author: joshu
 """
-
+from gensim.models import Word2Vec,LdaMulticore, TfidfModel
+from gensim import corpora
 from text_processor import *
 import pathlib
+import pandas as pd
 
 files = pathlib.Path('data')
 
@@ -18,4 +20,141 @@ for file in files.iterdir():
     with open(file, 'r',encoding = 'utf-8') as f:
         text.append(f.read())
     
-        
+df = pd.DataFrame()   
+
+df['titles'] = titles
+df['text'] = text     
+
+df.to_csv('corpus.csv')
+
+processed_text = []
+
+for i in text:
+    txt = clean_doc(i)
+    processed_text.append(txt)
+
+final_processed_text=[]
+
+for i in processed_text:
+    temp_DSI=i[0]
+    for k in range(1, len(i)):
+        temp_DSI= temp_DSI+ " "+i[k]
+    final_processed_text.append(temp_DSI)
+
+Tfidf=TfidfVectorizer(ngram_range=(1,3))
+TFIDF_matrix=Tfidf.fit_transform(final_processed_text)
+matrix=pd.DataFrame(TFIDF_matrix.toarray(), columns=Tfidf.get_feature_names(), index=titles)
+
+average_TFIDF={}
+for i in matrix.columns:
+    average_TFIDF[i]=np.mean(matrix[i])
+
+average_TFIDF_DF=pd.DataFrame(average_TFIDF,index=[0]).transpose()
+
+average_TFIDF_DF.columns=['TFIDF']
+
+#calculate Q1 and Q3 range
+Q1=np.percentile(average_TFIDF_DF, 25)
+Q3=np.percentile(average_TFIDF_DF, 75)
+IQR = Q3 - Q1
+outlier=Q3+(1.5*IQR)
+
+#words that exceed the Q3+IQR*1.5
+outlier_list=average_TFIDF_DF[average_TFIDF_DF['TFIDF']>=outlier]
+
+###############################################################################
+### Doc2Vec
+###############################################################################
+documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(final_processed_text)]
+model = Doc2Vec(documents, vector_size=100, window=2, min_count=1, workers=4)
+
+doc2vec_df=pd.DataFrame()
+for i in range(0,len(processed_text)):
+    vector=pd.DataFrame(model.infer_vector(processed_text[i])).transpose()
+    doc2vec_df=pd.concat([doc2vec_df,vector], axis=0)
+
+doc2vec_df=doc2vec_df.reset_index()
+
+doc_titles={'title': titles}
+t=pd.DataFrame(doc_titles)
+
+doc2vec_df=pd.concat([doc2vec_df,t], axis=1)
+
+doc2vec_df=doc2vec_df.drop('index', axis=1)
+
+###############################################################################
+### Gensim Word2vec 
+###############################################################################
+#word to vec
+model_w2v = Word2Vec(processed_text, vector_size=100, window=5, min_count=1, workers=4)
+
+#join all processed DSI words into single list
+processed_text_w2v=[]
+for i in processed_text:
+    for k in i:
+        processed_text_w2v.append(k)
+
+#obtian all the unique words from DSI
+w2v_words=list(set(processed_text_w2v))
+
+#can also use the get_feature_names() from TFIDF to get the list of words
+#w2v_words=Tfidf.get_feature_names()
+
+#empty dictionary to store words with vectors
+w2v_vectors={}
+
+#for loop to obtain weights for each word
+for i in w2v_words:
+    temp_vec=model_w2v.wv[i]
+    w2v_vectors[i]=temp_vec
+
+#create a final dataframe to view word vectors
+w2v_df=pd.DataFrame(w2v_vectors).transpose()
+
+
+#the following section runs applies the k-means algorithm on the TFIDF matrix.
+
+###############################################################################
+### K Means Clustering - TFIDF
+###############################################################################
+k=8
+km = KMeans(n_clusters=k, random_state =89)
+km.fit(TFIDF_matrix)
+clusters = km.labels_.tolist()
+
+
+terms = Tfidf.get_feature_names()
+Dictionary={'Doc Name':titles, 'Cluster':clusters,  'Text': final_processed_text}
+frame=pd.DataFrame(Dictionary, columns=['Cluster', 'Doc Name','Text'])
+
+print("Top terms per cluster:")
+#sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+
+terms_dict=[]
+
+
+#save the terms for each cluster and document to dictionaries.  To be used later
+#for plotting output.
+
+#dictionary to store terms and titles
+cluster_terms={}
+cluster_title={}
+
+
+for i in range(k):
+    print("Cluster %d:" % i),
+    temp_terms=[]
+    temp_titles=[]
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind])
+        terms_dict.append(terms[ind])
+        temp_terms.append(terms[ind])
+    cluster_terms[i]=temp_terms
+    
+    print("Cluster %d titles:" % i, end='')
+    temp=frame[frame['Cluster']==i]
+    for title in temp['Doc Name']:
+        print(' %s,' % title, end='')
+        temp_titles.append(title)
+    cluster_title[i]=temp_titles
